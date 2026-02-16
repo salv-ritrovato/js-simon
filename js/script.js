b51e652a0987011d5a469d505268c7754ec74bdd
salv-ritrovato <salv.ritrovato@gmail.com>
2026-02-16 15:00:33 +0100

    setInterval function, eventListener on forms, userNumbers inserted in an empty array

--- js/script.js
+++ js/script.js
@@ -13,16 +13,38 @@ for (let i = 0; i < 5; i++) {
 const cDown = document.getElementById('countdown')
 const nList = document.getElementById('numbers-list')
 const aForm = document.getElementById('answers-form')
+// Creo una variabile let che sarà il nostro countdown per i secondi
+let counter = 30;
 
 // Inserisco il contenuto dell'array all'interno di nList attraverso innerHTML
 // Utilizzo il metodo .join per aggiungere uno spazio fra ogni numeri per una questione di miglior visibilità
 nList.innerHTML = `<li>${numbersArray.join(' ')}</li>`;
 
-// Creo una funzione per far partire il countdown di 30 secondi e far sparire i numeri
-setTimeout(function() {
-     nList.classList.add('d-none');
-     aForm.classList.remove('d-none');
-}, 30000);
+// Creo una funzione setInterval per far scendere il countdown da 30 ad 1 ogni 1 sec e far apparire i form/sparire i numeri
+const timerStop = setInterval(function () {
+    if (counter === 0) {
+        clearInterval(timerStop)
+        nList.classList.add('d-none');
+        aForm.classList.remove('d-none');
+    } else {
+        counter--;
+        cDown.innerHTML = counter;
+    }
+}, 1000)
 
+// Aggancio aForm ad un eventListener per intercettare i dati inseriti dall'utente
+// Prevengo il ricaricamento dalla pagina che significherebbe perdere i dati inseriti dall'utente
+aForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+})
 
-                  
+// Prelevo gli input inseriti dall'utente
+// Creo una variabile const con array vuoto dove inserire i numeri dell'utente
+const userInputs = document.querySelectorAll('.form-control')
+const userNumbers = [];
+
+// For cycle per inserire gli input dell'utente all'interno dell'array vuoto in modo tale da poterli confrontare
+for (let i = 0; i < userInputs.length; i++) {
+    const inputValue = Number(userInputs[i].value);
+    userNumbers.push(inputValue);
+}
